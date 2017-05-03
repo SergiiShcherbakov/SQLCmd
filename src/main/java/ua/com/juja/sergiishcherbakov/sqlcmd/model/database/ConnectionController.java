bbb2341bbe0764a7913288f3d91f9818d9270d1c
SergiiShcherbakov <strannikmail@gmail.com>
2017-05-03 10:14:49 +0300

    refactoring after code review

--- src/main/java/ua/com/juja/sergiishcherbakov/sqlcmd/model/database/ConnectionController.java
+++ src/main/java/ua/com/juja/sergiishcherbakov/sqlcmd/model/database/ConnectionController.java
@@ -4,11 +4,12 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 /**
- * Created by StrannikFujitsu on 21.04.2017.
+ * Created by Sergii Shcherbakov on 21.04.2017.
  */
 public interface ConnectionController {
 
-    boolean setParameters(String databaseName, String login, String password) throws SQLException, ClassNotFoundException;
+    boolean setParameters(String databaseName, String login, String password)
+            throws SQLException, ClassNotFoundException;
 
     boolean Connect();
 
--- src/main/java/ua/com/juja/sergiishcherbakov/sqlcmd/model/database/DatabaseManager.java
+++ src/main/java/ua/com/juja/sergiishcherbakov/sqlcmd/model/database/DatabaseManager.java
@@ -1,20 +1,23 @@
 package ua.com.juja.sergiishcherbakov.sqlcmd.model.database;
 
 import ua.com.juja.sergiishcherbakov.sqlcmd.model.Field;
-
 import java.sql.SQLException;
 import java.util.List;
 
 /**
- * Created by StrannikFujitsu on 23.04.2017.
+ * Created by Sergii Shcherbakov on 23.04.2017.
  */
 public interface DatabaseManager {
-    boolean setConnection(String databaseName, String login, String password) throws SQLException, ClassNotFoundException;
+    boolean setConnection(String databaseName, String login, String password)
+            throws SQLException, ClassNotFoundException;
 
-    boolean createNewTable(String tableName, Field[] fields) throws SQLException, ClassNotFoundException;
+    boolean createNewTable(String tableName, Field[] fields)
+            throws SQLException, ClassNotFoundException;
 
-    boolean deleteTable(String tableName) throws SQLException, ClassNotFoundException;
+    boolean deleteTable(String tableName)
+            throws SQLException, ClassNotFoundException;
 
-    List<String> getTablesNames() throws SQLException, ClassNotFoundException;
+    List<String> getTablesNames()
+            throws SQLException, ClassNotFoundException;
 
 }
