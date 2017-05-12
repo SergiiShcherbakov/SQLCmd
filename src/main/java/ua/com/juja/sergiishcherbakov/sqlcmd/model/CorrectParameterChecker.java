55004989ff16dabe314929e1b8b361a388c9c8d0
SergiiShcherbakov <strannikmail@gmail.com>
2017-05-12 13:30:10 +0300

    Add documentation to method getCorrectNumberOfParameters in class CorrectParameterChecker

--- src/main/java/ua/com/juja/sergiishcherbakov/sqlcmd/model/CorrectParameterChecker.java
+++ src/main/java/ua/com/juja/sergiishcherbakov/sqlcmd/model/CorrectParameterChecker.java
@@ -5,8 +5,25 @@ import ua.com.juja.sergiishcherbakov.sqlcmd.model.exeptions.IncorrectNumberOfPar
 /**
  * Created by Sergii Shcherbakov on 11.05.2017.
  */
+
+
 public class CorrectParameterChecker {
-    public static String[] getCorrectNumberOfParameters(String goodParameter, String inputCommand, int countParameters) throws IncorrectNumberOfParametersException {
+
+    /**
+     * The method checks is correct parameters in input command.
+     * @param goodParameter - right first parameter.
+     * @param inputCommand - all parameters in one string with CVS separation. Separator "|".
+     * @param countParameters - right count of parameters.
+     * @return string array with right parameters.
+     * @throws IncorrectNumberOfParametersException
+     *       with message  "@param countParameters" parameters are expected but
+     *       "entered count parameters" is entered" if input wrong count parameters.
+     *       And with message
+     *       "@param goodParameter" parameters are expected but "bad first parameter"  is entered"
+     *       if input bad first parameter
+     */
+    public static String[] getCorrectNumberOfParameters(String goodParameter, String inputCommand, int countParameters)
+            throws IncorrectNumberOfParametersException {
         String[] data = inputCommand.split("[|]");
         if ( data.length != countParameters) {
             throw new IncorrectNumberOfParametersException( countParameters+ " parameters are expected but " +
