5ec90411282bfb8553c2c96c9f941e81a13c35e4
SergiiShcherbakov <strannikmail@gmail.com>
2017-05-12 14:16:41 +0300

    Change tables command and add unit tests

--- src/main/java/ua/com/juja/sergiishcherbakov/sqlcmd/controller/command/TablesCommand.java
+++ src/main/java/ua/com/juja/sergiishcherbakov/sqlcmd/controller/command/TablesCommand.java
@@ -1,6 +1,8 @@
 package ua.com.juja.sergiishcherbakov.sqlcmd.controller.command;
 
+import ua.com.juja.sergiishcherbakov.sqlcmd.model.CorrectParameterChecker;
 import ua.com.juja.sergiishcherbakov.sqlcmd.model.database.DatabaseManager;
+import ua.com.juja.sergiishcherbakov.sqlcmd.model.exeptions.IncorrectNumberOfParametersException;
 import ua.com.juja.sergiishcherbakov.sqlcmd.view.Viewer;
 import java.sql.SQLException;
 
@@ -18,11 +20,10 @@ public class TablesCommand extends CommandSkeleton implements Command  {
     public boolean processAndExit(Viewer viewer, DatabaseManager databaseManager,
                                   String inputCommand) {
         try {
+            CorrectParameterChecker.getCorrectNumberOfParameter(getName(), inputCommand);
             viewer.write(databaseManager.getTablesNames().toString());
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+        } catch (SQLException | IncorrectNumberOfParametersException | ClassNotFoundException e) {
+            viewer.write("tables cant be printed because " + e.getMessage());
         }
         return false;
     }
