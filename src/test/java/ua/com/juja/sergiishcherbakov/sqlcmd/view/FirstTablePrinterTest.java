96828e6c6f3294546d6c7df2b58e25a708cba34b
SergiiShcherbakov <strannikmail@gmail.com>
2017-05-14 15:25:02 +0300

    create class firstTablePrinter and test to it

---
+++ src/test/java/ua/com/juja/sergiishcherbakov/sqlcmd/view/FirstTablePrinterTest.java
@@ -0,0 +1,45 @@
+package ua.com.juja.sergiishcherbakov.sqlcmd.view;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.*;
+
+/**
+ * Created by Sergii Shcherbakov on 14.05.2017.
+ */
+public class FirstTablePrinterTest {
+    @Test
+    public void PrintTable() {
+        // given
+        ConsoleViewer consol = Mockito.mock(ConsoleViewer.class);
+        FirstTablePrinter printer = new FirstTablePrinter(consol);
+
+        List<List<String>> tab = new LinkedList<>();
+        for (int i = 0; i < 5; i++) {
+            List<String> row = new ArrayList<>();
+            String add = "";
+            for (int j = 0; j < 3; j++) {
+                add = "";
+                if(i % 2 == 0) add = "ttt";
+                row.add("testTab row " + (i+1) + " column " + (j+1) + add );
+            }
+            tab.add(row);
+        }
+        // when
+        printer.printTable(tab);
+        // then
+        Mockito.verify(consol).write(
+        "+-------------------------+-------------------------+-------------------------+" + System.lineSeparator() +
+        "|testTab row 1 column 1ttt|testTab row 1 column 2ttt|testTab row 1 column 3ttt|" + System.lineSeparator() +
+        "+-------------------------+-------------------------+-------------------------+" + System.lineSeparator() +
+        "|   testTab row 2 column 1|   testTab row 2 column 2|   testTab row 2 column 3|" + System.lineSeparator() +
+        "+-------------------------+-------------------------+-------------------------+" + System.lineSeparator() +
+        "|testTab row 3 column 1ttt|testTab row 3 column 2ttt|testTab row 3 column 3ttt|" + System.lineSeparator() +
+        "+-------------------------+-------------------------+-------------------------+" + System.lineSeparator() +
+        "|   testTab row 4 column 1|   testTab row 4 column 2|   testTab row 4 column 3|" + System.lineSeparator() +
+        "+-------------------------+-------------------------+-------------------------+" + System.lineSeparator() +
+        "|testTab row 5 column 1ttt|testTab row 5 column 2ttt|testTab row 5 column 3ttt|" + System.lineSeparator() +
+        "+-------------------------+-------------------------+-------------------------+" + System.lineSeparator()  );
+    }
+}
